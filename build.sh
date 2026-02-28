c0a662a23d15a2d04d5c95f473efa7ed4791f73b
JesterByte <ejjose94@gmail.com>
2026-03-01 01:50:44 +0800

    chore: add vercel configuration for deployment

---
+++ build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# 1. Install Flutter
+git clone https://github.com/flutter/flutter.git -b stable
+export PATH="$PATH:`pwd`/flutter/bin"
+
+# 2. Run Flutter build
+flutter config --enable-web
+flutter pub get
+flutter build web --release
