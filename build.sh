dc226aa1512ae52cf1bc3533431e9dab0e23660b
JesterByte <ejjose94@gmail.com>
2026-03-03 17:48:32 +0800

    chore: update build.sh for vercel .env

--- build.sh
+++ build.sh
@@ -4,7 +4,12 @@
 git clone https://github.com/flutter/flutter.git -b stable
 export PATH="$PATH:`pwd`/flutter/bin"
 
+cp .env.example .env
+
+sed -i "s|SUPABASE_URL|$SUPABASE_URL|g" .env
+sed -i "s|SUPABASE_ANON_KEY|$SUPABASE_ANON_KEY|g" .env
+
 # 2. Run Flutter build
 flutter config --enable-web
 flutter pub get
-flutter build web --release
+flutter build web --release 
